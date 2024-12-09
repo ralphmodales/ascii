03cfeb2bb8941bc6f05787be9d50cc3d28b4ba77
ralphmodales <ralphmodales@gmail.com>
2024-12-09 10:29:06 +0800

    Font changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const appleFont = localFont({
+  src: "./fonts/SF-Pro.woff2",
+  variable: "--font-sf-pro",
   weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  display: 'swap',
 });
 
 export const metadata = {
@@ -20,9 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${appleFont.variable} antialiased`}>
         {children}
       </body>
     </html>
